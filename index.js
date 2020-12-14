aafefe35856295758c04437eb9367c5de22a8e4e
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-14 13:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Deciduous extends Tree {
   
   static definition(){
     
-    return ${super.definition()} +` Deciduous trees shed their leaves annually.`;
+    return super.definition() +` Deciduous trees shed their leaves annually.`;
   }
 }
 
@@ -30,6 +30,6 @@ class Evergreen extends Tree {
   
   static definition(){
     
-    return ${super.definition()} +`Evergreens keep their leaves all year round.`;
+    return super.definition() +`Evergreens keep their leaves all year round.`;
   }
 }
